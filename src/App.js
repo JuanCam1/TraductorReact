38866339b57daef203356dab637c4708a8816339
Juan <juancamilor576@gmail.com>
2022-09-30 22:43:05 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
+import useTranslate from './hooks/useTranslate';
 import './App.css';
 
 function App() {
+  const {
+    data,
+    dataTra,
+    handleOnChange,
+    handleOnChangeSelecBefore,
+    handleOnChangeSelectAfter,
+    handleOnClick,
+  } = useTranslate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='translContainer'>
+        <h2>Traductor App</h2>
+
+        <div className='translBody'>
+          <div className='translBefore'>
+            <select onChange={handleOnChangeSelecBefore}>
+              <option value=''></option>
+              {data.map((item) => {
+                return (
+                  <option key={item.code} value={item.code}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+            <textarea onChange={handleOnChange} name='' id=''></textarea>
+          </div>
+
+          <div className='translAfter'>
+            <select onChange={handleOnChangeSelectAfter}>
+              <option value=' '></option>
+              {data.map((item) => {
+                return (
+                  <option key={item.code} value={item.code}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+            <textarea disabled value={dataTra}></textarea>
+          </div>
+        </div>
+        <div className='tranlsButton'>
+          <button onClick={handleOnClick}>Traducir</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/hooks/useTranslate.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react';
+
+const useTranslate = () => {
+  const [data, setData] = useState([]);
+  const [dataTra, setDataTra] = useState('');
+  const [textArea, setTextArea] = useState([]);
+  const [selectBefore, setSelectBefore] = useState('');
+  const [selectAfter, setSelectAfter] = useState('[]');
+
+
+  const url = 'https://text-translator2.p.rapidapi.com';
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '666c2e84d7mshc25cebd4090c0bap1acb93jsn47fbed07955c',
+        'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com',
+      },
+    };
+
+    fetch(`${url}/getLanguages`, options)
+      .then((response) => response.json())
+      .then(({ data }) => setData(data.languages))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setTextArea(value);
+  };
+  const handleOnChangeSelecBefore = (e) => {
+    const { value } = e.target;
+    setSelectBefore(value);
+  };
+  const handleOnChangeSelectAfter = (e) => {
+    const { value } = e.target;
+    setSelectAfter(value);
+  };
+
+  const handleOnClick = () => {
+    console.log(textArea);
+    console.log(selectBefore);
+    console.log(selectAfter);
+
+    if (selectBefore.length > 0 && selectAfter.length > 0) {
+      if (textArea.length > 0) {
+        const encodedParams = new URLSearchParams();
+        encodedParams.append('source_language', selectBefore);
+        encodedParams.append('target_language', selectAfter);
+        encodedParams.append('text', textArea);
+
+        const options = {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key':
+              '666c2e84d7mshc25cebd4090c0bap1acb93jsn47fbed07955c',
+            'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com',
+          },
+          body: encodedParams,
+        };
+
+        fetch(`${url}/translate`, options)
+          .then((response) => response.json())
+          .then(({ data }) => {
+            setDataTra(data.translatedText);
+          })
+          .catch((err) => console.error(err));
+      } else {
+        alert('digite el texto a traducir');
+      }
+    } else {
+      alert('seleccione los idiomas');
+    }
+
+    console.log(dataTra);
+  };
+
+
+  return {
+    data,
+    dataTra,
+    handleOnChange,
+    handleOnChangeSelecBefore,
+    handleOnChangeSelectAfter,
+    handleOnClick
+  }
+}
+
+export default useTranslate
